378e0079517ed6ad7f40bee91b1ab298ae59c95a
ka <ka.kaosf@gmail.com>
2012-12-03 23:34:46 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SyncHeroku::Application.config.secret_token = 'fb437e4deaf4ad54f423df9689f03c72041557ed77322090f8497c86438fddec0605ebe40c5b9dcf16d4a4a0e21ae70a824c577ae493de4019de4a1049c96cc6'
